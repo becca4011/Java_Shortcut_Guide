9e6eef4ddc25ecb388197b0b88a7c2c90ab706ee
becca4011 <becca4011@naver.com>
2020-11-24 22:03:10 +0900

    메인페이지 디자인

--- Shortcut_Guide/src/main_page/Mainpg.java
+++ Shortcut_Guide/src/main_page/Mainpg.java
@@ -6,23 +6,43 @@ import java.awt.event.*;
 
 public class Mainpg extends JFrame
 {
+	ImageIcon sc_bg;
+	ImageIcon sc_logo;
+	ImageIcon sc_dict_btn;
+	ImageIcon sc_dictroll_btn;
+	ImageIcon sc_srh_btn;
+	ImageIcon sc_srhroll_btn;
+	
 	public Mainpg()
 	{
 		setTitle("Shortcut Guide");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		sc_bg = new ImageIcon("image/shortcut_background.png");
+		sc_logo = new ImageIcon("image/shortcut_logo2.png");
+ 
+        JPanel background = new JPanel() 
+        {
+            public void paintComponent(Graphics g) 
+            {
+                g.drawImage(sc_bg.getImage(), 0, 0, getWidth(), getHeight(), null); // 배경 사진
+                g.drawImage(sc_logo.getImage(), 165, 155, null); // 로고
+                setOpaque(false); //그림을 표시하게 설정,투명하게 조절
+                super.paintComponent(g);
+            }
+        };
 		
-		Container c = getContentPane();
-		setLayout(null);
+        sc_dict_btn = new ImageIcon("image/shortcut_dictbtn.png");
+        sc_dictroll_btn = new ImageIcon("image/shortcut_dictbtn2.png");
+        
+		JButton dict = new JButton(sc_dict_btn);
+		dict.setPressedIcon(sc_dictroll_btn);
+		dict.setRolloverIcon(sc_dictroll_btn);
 		
-		JLabel logo = new JLabel("Shortcut Guide");
-		logo.setSize(800, 200);
-		logo.setFont(new Font("Calibri", Font.PLAIN, 100));
-		logo.setLocation(240, 120);
-		c.add(logo);
-		
-		JButton dict = new JButton("Dictionary");
-		dict.setSize(200, 40);
-		dict.setFont(new Font("Calibri", Font.PLAIN, 30));
+		dict.setBorderPainted(false);
+		dict.setContentAreaFilled(false);
+		dict.setSize(250, 70);
+		//dict.setFont(new Font("Calibri", Font.PLAIN, 30));
 		dict.setLocation(150, 450);
 		dict.addActionListener(new ActionListener() {
 			@Override
@@ -33,14 +53,24 @@ public class Mainpg extends JFrame
 				setVisible(false);
 			}
 		});
-		c.add(dict);
+		background.add(dict);
 		
-		JButton search = new JButton("Search");
-		search.setSize(200, 40);
-		search.setFont(new Font("Calibri", Font.PLAIN, 30));
-		search.setLocation(750, 450);
-		c.add(search);
+		sc_srh_btn = new ImageIcon("image/shortcut_srhbtn.png");
+        sc_srhroll_btn = new ImageIcon("image/shortcut_srhbtn2.png");
+        
+		JButton search = new JButton(sc_srh_btn);
+		search.setPressedIcon(sc_srhroll_btn);
+		search.setRolloverIcon(sc_srhroll_btn);
 		
+		search.setBorderPainted(false);
+		search.setContentAreaFilled(false);
+		search.setSize(250, 70);
+		//search.setFont(new Font("Calibri", Font.PLAIN, 30));
+		search.setLocation(700, 450);
+		background.add(search);
+		
+		setContentPane(background);
+		setLayout(null);
 		setSize(1100, 700);
 		setResizable(false); // 크기 변경 불가능하도록 함
 		setVisible(true);
